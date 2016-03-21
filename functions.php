147ab0accf3f7955c76d7a174cae888608265938
wolskiad <adamwolski0@gmail.com>
2016-03-21 01:58:00 -0400

    Revert "Test"
    This reverts commit e799a05c0d6e46315e0a31320b608e89ca07a945.

--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@ function lakefront_chophouse_setup() {
 	 * Let WordPress manage the document title.
 	 * By adding theme support, we declare that this theme does not use a
 	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us. The
+	 * provide it for us.
 	 */
 	add_theme_support( 'title-tag' );
 
