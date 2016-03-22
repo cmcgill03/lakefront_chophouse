6cd14c540a343ccb337c25872b7f38d6d189bfb9
wolskiad <wolskiad@users.noreply.github.com>
2016-03-21 21:40:28 -0400

    Comment for Page.php
    Added one additional comment

--- page.php
+++ page.php
@@ -16,7 +16,9 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+<!--
+* Calls the main content area of the header
+-->
 			<?php
 			while ( have_posts() ) : the_post();
 
