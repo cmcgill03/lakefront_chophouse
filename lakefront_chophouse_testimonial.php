a4a201bf94bd327b4e69a2a15bd1cf07cc408b6e
cmcgill03 <connor.mcgillivray@hotmail.com>
2016-03-23 16:04:30 -0400

    changing author details

---
+++ lakefront_chophouse_testimonial.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: Lakefront Chophouse Testimonial Plugin
+ * Plugin URI: 
+ * Description:
+ * Author: Adam, Cameron, Connor
+ * Author URI: http://phoenix.sheridanc.on.ca/~ccit3441/
+ * Version: 1.0
+*/
